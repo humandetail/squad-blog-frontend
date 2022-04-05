097a359ca4a5238b52f79e9cc6d12250df6e5054
laihanbin <512412344@qq.com>
2022-04-05 14:54:27 +0800

    feat(components): add common post widget

---
+++ components/common/CoverPic.vue
@@ -0,0 +1,58 @@
+<template>
+  <nuxt-link
+    v-if="to"
+    :to="to"
+    class="cover-pic-with-link"
+  >
+    <figure class="cover-pic">
+      <img :src="url">
+    </figure>
+  </nuxt-link>
+  <figure v-else class="cover-pic">
+    <img :src="url">
+  </figure>
+</template>
+
+<script setup lang="ts">
+import { RouteRecord } from 'vue-router'
+
+const props = withDefaults(defineProps<{
+  url: string;
+  to?: RouteRecord | string;
+  width?: string;
+  height?: string;
+}>(), {
+  to: '',
+  width: '440px',
+  height: '280px'
+})
+
+const { width, height } = props
+</script>
+
+<style lang="scss" scoped>
+$width: v-bind(width);
+$height: v-bind(height);
+
+.cover-pic-with-link {
+  width: $width;
+  height: $height;
+}
+
+.cover-pic {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: $width;
+  height: $height;
+  background-color: var(--img-background);
+
+  img {
+    display: block;
+    width: auto;
+    height: auto;
+    max-width: 100%;
+    max-height: 100%;
+  }
+}
+</style>
