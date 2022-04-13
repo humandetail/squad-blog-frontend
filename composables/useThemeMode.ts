b00192fcec961341462d3dcd9ae6b6d2d78e282c
laihanbin <512412344@qq.com>
2022-04-13 15:22:39 +0800

    chore(hooks): use default theme mode

--- composables/useThemeMode.ts
+++ composables/useThemeMode.ts
@@ -1,3 +1,3 @@
 import { Theme } from '~~/config/constants'
 
-export default () => useState<Theme>('themeMode', () => Theme.dark)
+export default () => useState<Theme>('themeMode', () => Theme.default)
