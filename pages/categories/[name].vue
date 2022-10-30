fbfc3de1e2e51b14fb8bf3e5b183195707125575
humandetail.com <512412344@qq.com>
2022-10-30 16:03:50 +0800

    feat: category page style

--- pages/categories/[name].vue
+++ pages/categories/[name].vue
@@ -54,3 +54,71 @@ const pagination = usePagination({
 
 onPaginationChange({ currentPage: 1, currentPageSize: 10 })
 </script>
+
+<style lang="scss" scoped>
+@import '~~/assets/styles/mixins.scss';
+
+.posts {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 24px;
+}
+
+.post-item-category {
+  display: block;
+  gap: unset;
+  width: calc(50% - 12px);
+  min-width: 0;
+  border-radius: var(--border-radius);
+  box-shadow: -3px -3px 6px var(--shadow-color-light), 3px 3px 6px var(--shadow-color-dark);
+
+  &:hover {
+    box-shadow: -3px -3px 6px var(--shadow-color-light), 3px 3px 6px var(--shadow-color-dark);
+  }
+
+  :deep(.cover-pic-with-link) {
+    display: block;
+    width: 100%;
+    .cover-pic {
+      border-top-left-radius: var(--border-radius);
+      border-top-right-radius: var(--border-radius);
+
+      img {
+        max-width: 100% !important;
+        max-height: 100% !important;
+      }
+    }
+  }
+
+  :deep(.title) {
+    display: flex;
+    align-items: center;
+    margin-top: var(--gap24);
+    padding: 0 var(--gap24);
+
+    a {
+      line-height: 1;
+      color: var(--primary-text);
+      @include ellipsis;
+
+      &:hover {
+        color: var(--primary-text);
+      }
+    }
+  }
+
+  :deep(.summary) {
+    line-height: 24px;
+    max-height: 48px;
+    margin-top: var(--gap24);
+    padding: 0 var(--gap24);
+    color: var(--secondary-text);
+    @include multi-ellipsis(2);
+  }
+
+  :deep(.widgets) {
+    margin: var(--gap24) 0;
+    padding: 0 var(--gap24);
+  }
+}
+</style>
