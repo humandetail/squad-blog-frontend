5a2e946e5add8f8cc3d3adb910c29d52d49bf706
humandetail.com <512412344@qq.com>
2022-11-09 21:57:28 +0800

    fix: error.vue

--- error.vue
+++ error.vue
@@ -1,10 +1,51 @@
 <template>
-  <Page404 />
+  <div class="page-not-found">
+    <h1 class="title">
+      404
+    </h1>
+
+    <h2 class="sub-title">
+      PAGE NOT FOUND.
+    </h2>
+
+    <div class="content">
+      <nuxt-link to="/search">
+        返回页面
+      </nuxt-link>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import Page404 from '~~/pages/404.vue'
 defineProps<{
   error: any
 }>()
 </script>
+
+<style lang="scss" scoped>
+.page-not-found {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: var(--gap32);
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  width: 300px;
+  height: 300px;
+  border-radius: 50%;
+  background-color: #fff;
+  transform: translate(-50%, -50%);
+
+  .title {
+    font-size: 40px;
+    color: var(--danger-color);
+  }
+
+  .sub-title {
+    font-size: 32px;
+    color: var(--danger-color);
+  }
+}
+</style>
