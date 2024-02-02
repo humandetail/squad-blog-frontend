afabf6285cca53d1c1ad16e668d5e21f7376e5f1
laihanbin <512412344@qq.com>
2024-02-02 11:32:38 +0800

    feat: about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,100 @@
 <template>
   <div class="about">
-    about
+    <div class="profile">
+      Hi, 这里是
+      <span class="nickname">{{ data.nickname ?? 'Detail' }}</span>
+    </div>
+
+    <div class="intro">
+      这家伙很懒，什么也没留下，可以通过下面的方式联系我
+    </div>
+
+    <ul class="contact">
+      <li
+        v-for="item of list"
+        :key="item.value"
+        class="item"
+      >
+        <CommonIcon
+          :icon="item.icon"
+          :size="2"
+        />
+        <component
+          :is="item.isLink ? 'a' : 'span'"
+          :href="item.value"
+          target="_blank"
+        >
+          {{ item.value }}
+        </component>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script setup lang="ts">
+import { getAboutUs } from '~/config/api'
+
+const { data } = await getAboutUs()
+
+const list = computed(() => {
+  return [
+    {
+      value: data.value.qq,
+      icon: 'qq'
+    },
+    {
+      value: data.value.email,
+      icon: 'mail'
+    },
+    {
+      value: data.value.github,
+      icon: 'github',
+      isLink: true
+    },
+  ]
+})
+
+useHead({
+  title: '关于我'
+})
+</script>
+
+<style lang="scss" scoped>
+.about {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: var(--gap-lg);
+  height: 100%;
+
+  .profile {
+    width: 30rem;
+    font-size: var(--font-size-lg);
+
+    .nickname {
+      color: var(--primary-color);
+      font-weight: 700;
+    }
+  }
+
+  .intro {
+    width: 30rem;
+    color: var(--secondary-text);
+  }
+
+  .contact {
+    width: 30rem;
+    .item {
+      display: flex;
+      align-items: center;
+      gap: var(--gap-sm);
+      padding: var(--gap-sm) 0;
+    }
+
+    .icon {
+      color: var(--secondary-text);
+    }
+  }
+}
+</style>
