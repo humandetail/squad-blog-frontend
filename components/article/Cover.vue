0d1498650ff4696ace5d5447c72d9a1dd7b267d1
laihanbin <512412344@qq.com>
2024-02-02 14:18:06 +0800

    feat: lazyload

--- components/article/Cover.vue
+++ components/article/Cover.vue
@@ -1,11 +1,13 @@
 <template>
   <figure
+    ref="figureRef"
     class="cover"
     @click="handleClick"
   >
     <img
       class="cover-img"
-      :src="cover.src"
+      :src="EmptyImage"
+      :data-src="cover.src"
       :alt="cover.alt ?? '封面图'"
     />
   </figure>
@@ -13,12 +15,40 @@
 
 <script setup lang="ts">
 import type { ArticleCover } from '~/types'
+import EmptyImage from '~/assets/empty.svg'
 
 const props = defineProps<{
   cover: ArticleCover
 }>()
 
 const router = useRouter()
+
+const figureRef = ref<HTMLElement | null>(null)
+let observer: IntersectionObserver | null = null
+
+onMounted(() => {
+  if (figureRef.value) {
+    observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const oImg = figureRef.value!.querySelector('.cover-img')
+          if (!oImg || oImg.classList.contains('loaded')) {
+            return
+          }
+          oImg.classList.add('loaded')
+          oImg.setAttribute('src', oImg.getAttribute('data-src') ?? '')
+          oImg.setAttribute('data-src', '')
+        }
+      })
+    })
+
+    observer.observe(figureRef.value)
+  }
+})
+
+onBeforeUnmount(() => {
+  observer?.disconnect?.()
+})
 
 const handleClick = () => {
   router.push(props.cover.route)
